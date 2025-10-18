81017d8400479efd444441fd02c022d51726006f
Ernesto Blejer <ernesto.blejer@gmail.com>
2025-10-18 20:29:32 -0300

    GET products

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola');
+// USAMOS > console.log(process.argv);
+// const method = process.argv[2];
+// console.log(method, resource);
+
+let [, , method, resource] = process.argv;
+
+method = method.toUpperCase();
+resource = resource.toLowerCase();
+
+if (method == 'GET' && resource == "products") {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const data = await response.json();
+  console.log(data);
+}
