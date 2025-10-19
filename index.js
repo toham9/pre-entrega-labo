6dbbcf1fb83b81b1f3757b80d78fe091bd875985
Ernesto Blejer <ernesto.blejer@gmail.com>
2025-10-19 11:10:27 -0300

    GET products/{id}

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ let [, , method, resource] = process.argv;
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
+// products/5
+if (method == 'GET' && resource.startsWith('products/')) {
+  let id = resource.split('/')[1];
+  id = parseInt(id);
+
+  if (isNaN(id) || id <= 0) {
+    console.log('No es un numero');
+  }
+  fetch('https://fakestoreapi.com/products/' + id)
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
 if (method == 'GET' && resource == "products") {
   const response = await fetch('https://fakestoreapi.com/products');
   const data = await response.json();
