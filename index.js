8e600230936b8a5a61471045e514890e23a44214
Ernesto Blejer <ernesto.blejer@gmail.com>
2025-10-19 12:56:31 -0300

    DELETE products/{id}

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ let [, , method, resource, ...params] = process.argv;
 method = method.toUpperCase();
 resource = resource.toLowerCase();
 
+// products/DELETE
+if (method == 'DELETE' && resource.startsWith('products/')) {
+      const id = resource.split('/')[1];
+
+  fetch(`https://fakestoreapi.com/products/${id}`, {
+   method: 'DELETE'
+  })
+  .then(response => response.json())
+  .then(data => console.log(data));
+}
+
 // products/PUT
 if (method == 'PUT' && resource.startsWith('products/')) {
   let id = resource.split('/')[1];
